6d4899f58bdec304ab998d2f5c65ccf129f98ab6
heuzin <matheusqs.arq@gmail.com>
2020-12-03 20:23:11 -0500

    function

---
+++ function.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    return a + b
+
+# This should print 7
+print(sum(2, 5))
+
+def double_the_sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    return 2 * sum(a, b)
+
+# This should print 14
+print(double_the_sum(2, 5))
